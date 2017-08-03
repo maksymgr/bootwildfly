df1adb31cda41d8adb6355d0a732627eb5941dad
Maksym Gryevtsov <maksymg@docupace.com>
2017-08-03 09:50:46 -0700

    no message

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,14 +1,18 @@
 package bootwildfly;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
+           
 
+    @Value("${MY_ENV}")
+    private String env;     
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+        return ("Hello, SpringBoot running in " + env);
     }
 }
